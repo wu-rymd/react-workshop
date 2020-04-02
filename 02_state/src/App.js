385d30db1fea7f04fbe9a4f6d778a368c5265ab8
wu-rymd <rjw415@nyu.edu>
2020-04-02 04:14:51 -0400

    add state

---
+++ 02_state/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar'
+import Content from './Content'
+import Clicker from './Clicker'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar websiteName="Website" />
+      <Content />
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
---
+++ 02_state/src/Clicker.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+const contentStyle = {
+    padding: 15,
+    border: "1px solid black",
+    margin: "20px 10px",
+    borderRadius: 5
+};
+
+class Clicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter : 0,
+        };
+        /* this.incrementCounter = this.incrementCounter.bind(this); */
+    }
+    
+    componentDidMount() {
+        console.log("Clicker mounted");
+    }
+
+    incrementCounter = () => {
+        console.log(this);
+        this.setState({
+            counter : ++this.state.counter
+        });
+    }
+
+    render() {
+        return (
+            <div style={contentStyle} onClick={this.incrementCounter}>
+              <h1>Stateful component</h1>
+              <h3>Click on this component to update its state.</h3>
+              <p> This component has been clicked: {this.state.counter} time
+                { Math.abs(this.state.counter) !== 1 ? "s" : "" }
+              </p>
+            </div>
+        );
+    }
+}
+
+export default Clicker;
