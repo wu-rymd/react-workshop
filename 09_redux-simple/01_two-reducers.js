fb2e2f4e0905d74bd453d4f11dcd55c139f737f8
wu-rymd <wu.raymond@columbia.edu>
2020-07-20 22:45:04 -0400

    refactor for essentials

--- 09_redux-simple/01_two-reducers.js
+++ 09_redux-simple/01_two-reducers.js
@@ -1,33 +1,3 @@
-// BARE BONES VERSION -- ONE REDUCER
-
-/*
-
-import { createStore } from "redux"
-
-const reducer = function(state, action) {
-    if (action.type === "INC")
-        return state + action.payload;
-    if (action.type === "DEC")
-        return state - action.payload;
-    return state
-}
-
-const store = createStore(reducer, 0);  // 0 is initial state
-
-store.subscribe( () => {
-    console.log("store changed", store.getState());
-});
-
-store.dispatch( {type: "INC", payload: 123} );
-store.dispatch( {type: "INC", payload: 234} );
-store.dispatch( {type: "DEC", payload:  12} );
-
-*/
-
-///////////////////////////////////////////////////////////////////////////
-
-// TWO REDUCERS
-
 import { createStore, combineReducers } from "redux"
 
 const INITIAL = {
---
+++ 09_redux-simple/02_middleware.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware } from "redux"
+
+const reducer = function(state, action) {
+    if (action.type === "INC")
+        return state + action.payload;
+    if (action.type === "DEC")
+        return state - action.payload;
+    return state
+}
+
+const logger = store => next => action => {
+    console.log("action fired", action);
+    // can modify action here...
+    next(action);  // dispatches action to reducer
+};
+
+const middleware = applyMiddleware(logger);
+
+const store = createStore(reducer, 0, middleware);  // 0 is initial state
+
+store.subscribe( () => {
+    console.log("store changed", store.getState());
+});
+
+store.dispatch( {type: "INC", payload: 123} );
+store.dispatch( {type: "INC", payload: 234} );
+store.dispatch( {type: "DEC", payload:  12} );
