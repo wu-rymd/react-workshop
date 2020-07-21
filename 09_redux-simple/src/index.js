698e4650e63772f1b985c87ed5b5bc27bff45a64
wu-rymd <wu.raymond@columbia.edu>
2020-07-20 22:05:13 -0400

    add multiple reducers version

--- 09_redux-simple/src/index.js
+++ 09_redux-simple/src/index.js
@@ -1,4 +1,8 @@
-import { createStore } from "redux";
+// BARE BONES VERSION -- ONE REDUCER
+
+/*
+
+import { createStore } from "redux"
 
 const reducer = function(state, action) {
     if (action.type === "INC")
@@ -17,3 +21,46 @@ store.subscribe( () => {
 store.dispatch( {type: "INC", payload: 123} );
 store.dispatch( {type: "INC", payload: 234} );
 store.dispatch( {type: "DEC", payload:  12} );
+
+*/
+
+///////////////////////////////////////////////////////////////////////////
+
+// TWO REDUCERS
+
+import { createStore, combineReducers } from "redux"
+
+const INITIAL = {
+    name: "Bob",
+    pets: []
+}
+
+// initial state now defined in function signature
+const nameReducer = function(state=INITIAL.name, action) {
+    if (action.type === "CHANGE_NAME")
+        return state = action.payload;
+    return state;
+}
+
+const petReducer = function(state=INITIAL.pets, action) {
+    if (action.type === "ADD_PET")
+        return [...state, action.payload]  // creates new list
+    return state;
+}
+
+const reducers = combineReducers({
+    name: nameReducer,
+    pets: petReducer
+});
+
+const store = createStore(reducers);
+
+store.subscribe( () => {
+    console.log("store changed", store.getState());
+});
+
+store.dispatch( {type: "CHANGE_NAME", payload: "John"} );
+store.dispatch( {type: "ADD_PET", payload: "Samuel the Cat"} );
+store.dispatch( {type: "ADD_PET", payload: "Frosty the Snowman"} );
+store.dispatch( {type: "CHANGE_NAME", payload: "Ben"} );
+store.dispatch( {type: "ADD_PET", payload: "Casper the Ghost"} );
