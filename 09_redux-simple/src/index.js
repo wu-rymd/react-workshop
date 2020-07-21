0b6e8cdbc548d5407cc5397926030fc42c5ae64f
wu-rymd <wu.raymond@columbia.edu>
2020-07-20 20:16:55 -0400

    add simple redux start kit

--- 09_redux-simple/src/index.js
+++ 09_redux-simple/src/index.js
@@ -1,11 +1,11 @@
 import { createStore } from "redux";
 
-const reducer = function(currState, action) {
+const reducer = function(state, action) {
     if (action.type === "INC")
-        return currState + action.payload;
+        return state + action.payload;
     if (action.type === "DEC")
-        return currState - action.payload;
-    return currState
+        return state - action.payload;
+    return state
 }
 
 const store = createStore(reducer, 0);  // 0 is initial state
